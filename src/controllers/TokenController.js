228345395a8c187ccfe421b6a2979b0d3cb0020d
Jose <ajose9975@gmail.com>
2022-08-08 17:33:00 -0300

    Add JWT Token

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class TokenController {
+  async store(req, res) {
+    const { email = '', password = '' } = req.body; // Pega o email e senha da requisição
+
+    if (!email || !password) { // Checa se faltou algum
+      return res.status(401).json({
+        errors: ['Credenciais inválidas'],
+      });
+    }
+
+    // Busca no BD o usuario com o email correspondente
+    const user = await User.findOne({ where: { email } });
+
+    // Checa se o usuário existe no BD
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário não existe'],
+      });
+    }
+    // Se a senha da req não for a mesma encriptada no BD, não é valida
+    if (!(await user.passIsValid(password))) {
+      return res.status(401).json({
+        errors: ['Senha Inválida'],
+      });
+    }
+
+    const { id } = user;
+
+    // Cria o token baseado no ID(Extraido do usuario acima)
+    // E no Email (Passado no corpo da req)
+    const token = jwt.sign(
+      { id, email },
+      process.env.TOKEN_SECRET,
+      { expiresIn: process.env.TOKEN_LIFETIME },
+    );
+
+    return res.json({ token });
+  }
+}
+
+export default new TokenController();
